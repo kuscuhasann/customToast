5bd86d0603ecbac650014f23ee3b8702f224585d
kuscuhasann <kuscuhasann97@gmail.com>
2022-06-12 16:48:43 +0300

    custom toast finished

--- app/src/main/java/com/pakt_games/customtoast/MainActivity.kt
+++ app/src/main/java/com/pakt_games/customtoast/MainActivity.kt
@@ -3,6 +3,7 @@ package com.pakt_games.customtoast
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.widget.TextView
 import android.widget.Toast
 import com.pakt_games.customtoast.databinding.ActivityMainBinding
 
@@ -16,10 +17,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(bindingMain.root)
 
         bindingMain.buttonShowToast.setOnClickListener {
-            val toast = Toast(this)
-            val view = LayoutInflater.from(this).inflate(R.layout.custom_toast_layout, null)
-            toast.view = view
-            toast.show()
+            showToast()
         }
+    }
+
+    private fun showToast() {
+        val toast = Toast(this)
+        val view = LayoutInflater.from(this).inflate(R.layout.custom_toast_layout, null)
+        val textView = view.findViewById<TextView>(R.id.textViewToastMessage)
+        textView.text = "Hasan Kuşçu"
+        toast.view = view
+        toast.show()
     }
 }
